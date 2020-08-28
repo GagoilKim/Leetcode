0bad35262073bd221650843170db2cb1cba040e9
Gagoilkim <dkim558@wisc.edu>
2020-08-28 17:45:21 -0500

    need to fix

---
+++ sortedArrayToBST.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        center = int(len(nums)/ 2)
+        root = TreeNode()
+        root.val = nums.pop(center)
+        
+        for i in range(center):
+# left
+                    
+    
+# right
+            
+        
+        return root
+        
+        
