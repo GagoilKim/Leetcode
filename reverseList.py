d4302627132e29551353dfa13cd7b79fc554fa5a
Gagoilkim <dkim558@wisc.edu>
2020-09-05 17:18:55 -0500

    complete

--- reverseList.py
+++ reverseList.py
@@ -6,21 +6,24 @@
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         vals = []
-        if head.next == None:
+        if head == None:
+            return None
+        elif head.next == None:
             return head
-        while head.next != None:
-            v = head.val
-            vals.append(v)
-            head = head.next
-        vals.append(head.val)
-        
-        v = vals.reverse()
-        result = ListNode(0, None)
-        tmp = result
-        for i in vals:
-            cur = ListNode(i, None)
-            tmp.next = cur
-            tmp = tmp.next
-            
-            
-        return result.next
+        else:
+            while head.next != None:
+                v = head.val
+                vals.append(v)
+                head = head.next
+            vals.append(head.val)
+
+            v = vals.reverse()
+            result = ListNode(0, None)
+            tmp = result
+            for i in vals:
+                cur = ListNode(i, None)
+                tmp.next = cur
+                tmp = tmp.next
+
+
+            return result.next
