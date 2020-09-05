4f2195c252f4ff71c89c89dc70ab1a02f6355f8e
Gagoilkim <dkim558@wisc.edu>
2020-09-05 17:00:30 -0500

    need to fix

---
+++ reverseList.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        vals = []
+        if head.next == None:
+            return head
+        while head.next != None:
+            v = head.val
+            vals.append(v)
+            head = head.next
+        vals.append(head.val)
+        
+        v = vals.reverse()
+        result = ListNode(0, None)
+        tmp = result
+        for i in vals:
+            cur = ListNode(i, None)
+            tmp.next = cur
+            tmp = tmp.next
+            
+            
+        return result.next
